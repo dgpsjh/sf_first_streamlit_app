4df068612e403cd8d5b51f210450a28710b6c979
dgpsjh <125687761+dgpsjh@users.noreply.github.com>
2023-03-10 16:30:06 -0800

    Fix Python script - user the proper Comment specifier "#".

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,6 @@ streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
---Pull data into a Pandas Dataframe called my_fruit_list - Then ask the Streamlist library to display it on the page:
+#Pull data into a Pandas Dataframe called my_fruit_list - Then ask the Streamlist library to display it on the page:
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
