0d4144d2409e9d8c8d24520393eb01d52efdd7c5
dgpsjh <125687761+dgpsjh@users.noreply.github.com>
2023-03-10 16:28:47 -0800

    Import Pandas library - Panda functions will read data & display on page.
    1. my_fruit_list = pandas.read_csv("xxx")
    2. streamlit.dataframe(my_fruit_list)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,16 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+--Pull data into a Pandas Dataframe called my_fruit_list - Then ask the Streamlist library to display it on the page:
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
